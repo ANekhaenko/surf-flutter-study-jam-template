e09edaf9daf181cf03f34d8c3eedbdc36a2db5ff
Anekhaenko <andrew.unreal@gmail.com>
2022-02-20 18:06:19 +0200

    test

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -16,9 +16,50 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  Widget MyList() {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text(widget.chatRepository.messages.toString()),
+          leading: Icon(Icons.ac_unit),
+        ),
+      ],
+    );
+  }
+
+  // Widget listOfMessages() {
+  //   final List<String> items = widget.chatRepository.toString();
+  //   return ListView();
+  // }
+
+  void _refreshChat() {
+    widget.chatRepository.toString();
+    print('sadda');
+  }
+
+  final textConroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    // TODO(task): Use ChatRepository to implement the chat.
+    return (Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: textConroller,
+          decoration: const InputDecoration(
+            hintText: 'Введите ник',
+            hintStyle: TextStyle(color: Colors.white),
+          ),
+          style: const TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () => {_refreshChat}, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: SafeArea(
+          child: Container(
+        child: MyList(),
+      )),
+    ));
     throw UnimplementedError();
   }
 }
